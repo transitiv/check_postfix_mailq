ccaea5c38191949a456d8b973b01d61ed2105d45
Armin Pech <github@arminpech.de>
2019-11-08 12:15:24 +0100

    performance optimizations

--- check_postfix_mailq.py
+++ check_postfix_mailq.py
@@ -4,20 +4,21 @@
 # For details see: $0 --help
 
 import __future__
-import sys
+from sys import argv, exit
 from os import linesep
-import argparse
-import re
+from argparse import ArgumentParser
+from re import compile, search, IGNORECASE
+from collections import Counter
 from subprocess import check_output, CalledProcessError
 
 def default_re_email():
     return '[a-z0-9\.-]{1,64}|[a-z0-9\.-]{0,64}@[a-z0-9\.-]{2,254}'
 
 def validate_email(input):
-    pattern=re.compile('^(%s)$' % default_re_email(), re.IGNORECASE)
+    pattern=compile('^(%s)$' % default_re_email(), IGNORECASE)
     if not pattern.match(input):
         print('UNKNOWN: Failed to validate sender filter, expecting "%s"' % default_re_email())
-        sys.exit(3)
+        exit(3)
     if input[0] == '@':
         input = '.*%s' % input
     return input
@@ -25,39 +26,33 @@ def validate_email(input):
 def validate_int(input):
     if int(input) < 0:
         print('UNKNOWN: Integer parameters must not be negative')
-        sys.exit(3)
+        exit(3)
     return int(input)
 
 def check_mailq(input, sender_filter, perfdata_details, count_warning, count_critical, size_warning, size_critical):
-    mailq_count = {}
-    mailq_size = {}
-    mailq_recipients = {}
+    mailq_count = Counter()
+    mailq_size = Counter()
+    mailq_recipients = Counter()
     current_sender = None
 
-    re_sender = re.compile('^[0-9A-F]{10}([!\*]?)\s+([0-9]+)\s+.*\s+(%s)$' % sender_filter, re.IGNORECASE)
-    re_recipient = re.compile('^\s+(%s)$' % default_re_email())
+    re_sender = compile('^[0-9A-F]{10}([!\*]?)\s+([0-9]+)\s+.*\s+(%s)$' % sender_filter, IGNORECASE)
+    re_recipient = compile('^\s+(%s)$' % default_re_email())
 
     for line in input.decode('utf-8').split('\n'):
-        sender_match = re.search(re_sender, line.rstrip(linesep))
-        if sender_match:
-            current_sender = sender_match.group(3)
-            try:
-                mailq_count[current_sender] += 1
-            except KeyError:
-                mailq_count[current_sender] = 1
-            try:
-                mailq_size[current_sender] += int(sender_match.group(2))
-            except KeyError:
-                mailq_size[current_sender] = int(sender_match.group(2))
-        else:
-            recipient_match = re.search(re_recipient, line.rstrip(linesep))
-            if recipient_match:
-                try:
-                    mailq_recipients[current_sender] += 1
-                except KeyError:
-                    mailq_recipients[current_sender] = 1
+        line = line.rstrip(linesep)
+        if len(line) > 0:
+            if search(re_recipient, line):
+                mailq_recipients[current_sender] += 1
+            else:
+                sender_match = search(re_sender, line)
+                if sender_match:
+                    current_sender = sender_match.group(3)
+                    mailq_count[current_sender] += 1
+                    mailq_size[current_sender] += int(sender_match.group(2))
 
-    perfdata = ['count=%i;%i;%i;;' % (sum(mailq_count.values()), count_warning, count_critical), 'size=%iB;%i;%i;;' % (sum(mailq_size.values()), size_warning, size_critical)]
+    sum_mailq_count = sum(mailq_count.values())
+    sum_mailq_size = sum(mailq_size.values())
+    perfdata = ['count=%i;%i;%i;;' % (sum_mailq_count, count_warning, count_critical), 'size=%iB;%i;%i;;' % (sum_mailq_size, size_warning, size_critical)]
     if perfdata_details:
         for k, v in mailq_count.items():
             perfdata += ['count[%s]=%i' % (k, v)]
@@ -67,52 +62,52 @@ def check_mailq(input, sender_filter, perfdata_details, count_warning, count_cri
             perfdata += ['recipients[%s]=%i' % (k, v)]
     perfdata = ' '.join(sorted(perfdata))
 
-    if sum(mailq_count.values()) >= count_critical:
+    if sum_mailq_count >= count_critical:
         return 2, 'CRITICAL: mailq count >%i | %s' % (count_critical, perfdata)
 
-    if sum(mailq_count.values()) >= count_warning:
+    if sum_mailq_count >= count_warning:
         return 1, 'WARNING: mailq count >%i | %s' % (count_warning, perfdata)
 
-    if size_critical > 0 and sum(mailq_size.values()) >= size_critical:
+    if size_critical > 0 and sum_mailq_size >= size_critical:
         return 2, 'CRITICAL: mailq size >%i | %s' % (size_critical, perfdata)
 
-    if size_warning > 0 and sum(mailq_size.values()) >= size_critical:
+    if size_warning > 0 and sum_mailq_size >= size_critical:
         return 1, 'WARNING: mailq size >%i | %s' % (size_critical, perfdata)
 
     return 0, 'OKAY: mailq count and size okay | %s' % perfdata
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Check postfix mailq nagios/icinga plugins')
+    parser = ArgumentParser(description='Check postfix mailq nagios/icinga plugins')
     parser.add_argument('--sender-filter', type=validate_email, required=False, help='Check only mailq entries which given email address or domain, e.g. "noreply@example.com" or "@example.net')
     parser.add_argument('--count-warning', type=validate_int, required=True, help="Generate warning if mailq entries exceeds this threshold")
     parser.add_argument('--count-critical', type=validate_int, required=True, help="Generate critical if mailq entries exceeds this threshold")
     parser.add_argument('--size-warning', type=validate_int, default=0, required=False, help="Generate warning if size in bytes of mails in mailq exceeds this threshold")
     parser.add_argument('--size-critical', type=validate_int, default=0, required=False, help="Generate critical if size in bytes of mails in mailq exceeds this threshold")
     parser.add_argument('--perfdata-details', action='store_true', help='Print details about single sender addresses in perfdata')
-    args = parser.parse_args(sys.argv[1:])
+    args = parser.parse_args(argv[1:])
 
     if args.count_warning >= args.count_critical:
         print('UNKNOWN: count warning must be greater than critical')
-        sys.exit(3)
+        exit(3)
 
     if args.size_warning > 0 and args.size_warning >= args.size_critical:
         print('UNKNOWN: size warning must be greater than critical')
-        sys.exit(3)
+        exit(3)
 
     try:
         sender_filter = args.sender_filter if args.sender_filter else default_re_email()
-        re.compile(sender_filter, re.IGNORECASE)
+        compile(sender_filter, IGNORECASE)
     except:
         print('UNKNOWN: Failed to compile regex to match mailq entries')
-        sys.exit(3)
+        exit(3)
 
     try:
         mailq = check_output(['mailq'])
     except CalledProcessError:
         # check_output already wrote to STDERR - just exit here
-        sys.exit(3)
+        exit(3)
 
     ret = check_mailq(mailq, sender_filter, args.perfdata_details,args.count_warning, args.count_critical, args.size_warning, args.size_critical)
     print(ret[1])
-    sys.exit(ret[0])
+    exit(ret[0])
